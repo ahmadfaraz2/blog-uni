12cfc8ef9f65d98723d79b40c3b35f1a0c2a71a1
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2024-12-03 08:48:15 +0500

    Add a similiar posts recommending functionality

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .forms import EmailPostForm, CommentForm
 
 from taggit.models import Tag
+from django.db.models import Count
 
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
@@ -44,7 +45,17 @@ def post_detail(request, year, month, day, post):
     # Form for users to comment
     form = CommentForm()
 
-    return render(request, "blog/post/detail.html", {"post": post, "comments":comments, "form":form})
+    # List of similar posts
+    post_tags_id = post.tags.values_list("id", flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_id).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by("-same_tags", "-publish")[:4]
+
+    return render(request,
+                "blog/post/detail.html",
+                {"post": post, 
+                "comments": comments, 
+                "form": form,
+                "similar_posts": similar_posts})
 
 
 def post_share(request, post_id):
